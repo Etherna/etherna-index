8e994974755f206770b328655929dce14feee1a5
Mirko Da Corte <mirko@etherna.io>
2021-06-13 14:59:25 +0200

    Updated gitversion Minor updates

--- src/EthernaIndex.Persistence/GlobalSuppressions.cs
+++ src/EthernaIndex.Persistence/GlobalSuppressions.cs
@@ -5,4 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposable objects are instanced and returned by properties", Scope = "NamespaceAndDescendants", Target = "Etherna.EthernaIndex.Persistence.ModelMaps")]
+[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposable objects are instanced and returned by properties", Scope = "NamespaceAndDescendants", Target = "~N:Etherna.EthernaIndex.Persistence.ModelMaps")]
