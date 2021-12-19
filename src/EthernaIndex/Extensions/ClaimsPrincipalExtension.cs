dab4591d7d66805f9d15e142fff1f1a521b74c86
Federico Cicciarella <federico.cicciarella@gmail.com>
2021-12-19 14:11:56 +0100

    add user role and administrator claim

--- src/EthernaIndex/Extensions/ClaimsPrincipalExtension.cs
+++ src/EthernaIndex/Extensions/ClaimsPrincipalExtension.cs
@@ -12,7 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-using Etherna.EthernaIndex.Consts;
+using Etherna.EthernaIndex.Configs;
 using System;
 using System.Linq;
 using System.Security.Claims;
