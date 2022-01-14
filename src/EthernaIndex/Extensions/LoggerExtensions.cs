825db06cb5df843dd7d4d572550640961909d859
Federico Cicciarella <federico.cicciarella@gmail.com>
2022-01-14 20:35:50 +0100

    VideosControllerService logger info

--- src/EthernaIndex/Extensions/LoggerExtensions.cs
+++ src/EthernaIndex/Extensions/LoggerExtensions.cs
@@ -16,6 +16,35 @@ namespace Etherna.EthernaIndex.Extensions
         //*** DEBUG LOGS ***
 
         //*** INFORMATION LOGS ***
+        private static readonly Action<ILogger, string, string, Exception> _createdCommentVideo =
+            LoggerMessage.Define<string, string>(
+                LogLevel.Information,
+                new EventId(2, nameof(CreatedCommentVideo)),
+                "User Id '{Id}' created new comment for video with hash {ManifestHash}");
+
+        private static readonly Action<ILogger, string, string, Exception> _createdVideo =
+            LoggerMessage.Define<string, string>(
+                LogLevel.Information,
+                new EventId(2, nameof(CreatedVideo)),
+                "User Id '{Id}' created new video with hash {ManifestHash}");
+
+        private static readonly Action<ILogger, string, Exception> _deletedVideo =
+            LoggerMessage.Define<string>(
+                LogLevel.Information,
+                new EventId(2, nameof(DeletedVideo)),
+                "Video with hash {ManifestHash} deleted by author");
+
+        private static readonly Action<ILogger, string, string, Exception> _votedVideo =
+            LoggerMessage.Define<string, string>(
+                LogLevel.Information,
+                new EventId(2, nameof(CreatedCommentVideo)),
+                "User Id '{Id}' voted video with hash {ManifestHash}");
+
+        private static readonly Action<ILogger, string, string, Exception> _updatedVideo =
+            LoggerMessage.Define<string, string>(
+                LogLevel.Information,
+                new EventId(2, nameof(CreatedVideo)),
+                "Video hash {OldHash} updated with {NewHash} by author");
 
         //*** WARNING LOGS ***
 
@@ -27,6 +56,21 @@ namespace Etherna.EthernaIndex.Extensions
                 "Request {RequestId} throwed error");
 
         // Methods.
+        public static void CreatedCommentVideo(this ILogger logger, string id, string manifestHash) =>
+            _createdCommentVideo(logger, id, manifestHash, null!);
+
+        public static void CreatedVideo(this ILogger logger, string id, string manifestHash) =>
+            _createdVideo(logger, id, manifestHash, null!);
+
+        public static void DeletedVideo(this ILogger logger, string manifestHash) =>
+            _deletedVideo(logger, manifestHash, null!);
+
+        public static void VotedVideo(this ILogger logger, string id, string manifestHash) =>
+            _votedVideo(logger, id, manifestHash, null!);
+
+        public static void UpdatedVideo(this ILogger logger, string oldHash, string newHash) =>
+            _updatedVideo(logger, oldHash, newHash, null!);
+
         public static void RequestThrowedError(this ILogger logger, string requestId) =>
             _requestThrowedError(logger, requestId, null!);
     }
