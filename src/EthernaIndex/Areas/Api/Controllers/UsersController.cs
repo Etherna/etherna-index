084be4db439eb158e2a2c2feba4dd38a1f0064ec
Mirko Da Corte <mirko@etherna.io>
2022-02-21 19:12:08 +0100

    update api version to v0.3

--- src/EthernaIndex/Areas/Api/Controllers/UsersController.cs
+++ src/EthernaIndex/Areas/Api/Controllers/UsersController.cs
@@ -26,7 +26,7 @@ using System.Threading.Tasks;
 namespace Etherna.EthernaIndex.Areas.Api.Controllers
 {
     [ApiController]
-    [ApiVersion("0.2")]
+    [ApiVersion("0.3")]
     [Route("api/v{api-version:apiVersion}/[controller]")]
     public class UsersController : ControllerBase
     {
