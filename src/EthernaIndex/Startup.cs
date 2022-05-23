4ae1997775b9610b23080b2d68355c073b8714ed
Mirko Da Corte <mirko@etherna.io>
2022-05-23 23:55:38 +0200

    update swagger config

--- src/EthernaIndex/Startup.cs
+++ src/EthernaIndex/Startup.cs
@@ -227,6 +227,8 @@ namespace Etherna.EthernaIndex
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             services.AddSwaggerGen(options =>
             {
+                options.SupportNonNullableReferenceTypes();
+
                 //add a custom operation filter which sets default values
                 options.OperationFilter<SwaggerDefaultValues>();
 
