add7f14e768b16d488004745e3e651ac973be7e1
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-09-03 08:49:22 +0200

    refactory

--- src/EthernaIndex/Areas/Api/Services/ModerationControllerService.cs
+++ src/EthernaIndex/Areas/Api/Services/ModerationControllerService.cs
@@ -12,7 +12,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-using Etherna.Authentication;
 using Etherna.EthernaIndex.Domain;
 using Etherna.EthernaIndex.Services.Domain;
 using Etherna.EthernaIndex.Services.Extensions;
